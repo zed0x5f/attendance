ca4e772e0a92da99b6c7af1553479b7cee029552
jake <jst4nt0n@gmail.com>
2022-02-16 14:30:18 -0500

    input field has been added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,25 +2,27 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminHomeComponent } from './admin/home/home.component';
 import { ImportUsersComponent } from './admin/import-users/import-users.component';
+import { ConsoleCheckinComponent } from './console-checkin/console-checkin.component';
 import { HomeComponent } from './home/home.component';
 import { MealAttendanceComponent } from './meal-attendance/meal-attendance.component';
 import { MealFormTemplateDrivenComponent } from './meal-form-template-driven/meal-form-template-driven.component';
 
 //helper function
-let makeRoute = (pathI:string,componentI:any)=>{
-  return {path:pathI,component:componentI};
-}
+let makeRoute = (pathI: string, componentI: any) => {
+  return { path: pathI, component: componentI };
+};
 
 const routes: Routes = [
-  { path: '', component: HomeComponent},
-  makeRoute("meal",MealAttendanceComponent),
-  makeRoute("meal2",MealFormTemplateDrivenComponent),
-  makeRoute("admin",AdminHomeComponent),
-  makeRoute("admin/import-users",ImportUsersComponent),
+  { path: '', component: HomeComponent },
+  makeRoute('meal', MealAttendanceComponent),
+  makeRoute('console', ConsoleCheckinComponent),
+  makeRoute('meal2', MealFormTemplateDrivenComponent),
+  makeRoute('admin', AdminHomeComponent),
+  makeRoute('admin/import-users', ImportUsersComponent),
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/console-checkin/console-checkin.component.ts
+++ src/app/console-checkin/console-checkin.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-console-checkin',
   templateUrl: './console-checkin.component.html',
-  styleUrls: ['./console-checkin.component.scss']
+  styleUrls: ['./console-checkin.component.scss'],
 })
 export class ConsoleCheckinComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  value = '';
+  update(value: string) {
+    this.value = value;
   }
-
 }
